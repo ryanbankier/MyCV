62ab4da018e97808fc93f0fee503da921fd78ceb
ryanbankier <68638674+ryanbankier@users.noreply.github.com>
2022-04-21 10:56:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $(".show-email").after($("<p>").attr({class:"email-text"}).html("ryanbankier88@gmail.com").hide());
-
+    /* hides or shows the infomation depending on which icon is clicked*/
     $(".icon-exp").click(function(){
         $('.exp-text').slideToggle("slow");
     });
@@ -11,21 +11,24 @@ $(document).ready(function() {
     $(".icon-hob").click(function(){
         $('.hob-text').slideToggle("slow");
     });
-
+    
+    /* animates each icon if the mouse moves over it*/
     $(".icons-ex").mouseover(function(){
         $(this).animate({"width":"75px"},"fast")
     });
     $(".icons-ex").mouseleave(function(){
         $(this).animate({"width":"65px"},"fast")
     });
-
+    
+    /* shows the email address if the mouse is over the email link*/
     $(".show-email").mouseover(function(){
         $(".email-text").slideToggle()
     });
     $(".show-email").mouseleave(function(){
         $(".email-text").slideToggle()
     });
-
+    
+    /* shows/ hides the scroll bar depending which section the mouse is over*/
     $(".vLine").mouseenter(function(){
         $(this).toggleClass("scrollTogg")
     });
