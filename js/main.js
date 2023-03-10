4e21e95e1e73394439b266b5b899285d2e125777
ryanbankier <ryanbankier@yahoo.co.uk>
2023-03-10 08:12:01 +0200

    cv update 03-10

--- js/main.js
+++ js/main.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function() {
-
-    $(".show-email").after($("<p>").attr({class:"email-text"}).html("ryanbankier88@gmail.com").hide());
-
-    $(".icon-exp").click(function(){
-        $('.exp-text').slideToggle("slow");
-    });
-    $(".icon-edu").click(function(){
-        $('.edu-text').slideToggle("slow");
-    });
-    $(".icon-hob").click(function(){
-        $('.hob-text').slideToggle("slow");
-    });
-
-    $(".icons-ex").mouseover(function(){
-        $(this).animate({"width":"75px"},"fast")
-    });
-    $(".icons-ex").mouseleave(function(){
-        $(this).animate({"width":"65px"},"fast")
-    });
-
-    $(".show-email").mouseover(function(){
-        $(".email-text").slideToggle()
-    });
-    $(".show-email").mouseleave(function(){
-        $(".email-text").slideToggle()
-    });
-
-    $(".vLine").mouseenter(function(){
-        $(this).toggleClass("scrollTogg")
-    });
-    $(".vLine").mouseleave(function(){
-        $(this).toggleClass("scrollTogg")
-    });
-
-    $("#xpCol").mouseenter(function(){
-        $(this).toggleClass("scrollTogg1")
-    });
-    $("#xpCol").mouseleave(function(){
-        $(this).toggleClass("scrollTogg1")
-    });
-
-    $(".show-email").mouseleave(function(){
-        $(".email-text").slideToggle()
-    });
-
-});
-=======
 $(document).ready(function() {
 
     $(".show-email").after($("<p>").attr({class:"email-text"}).html("ryanbankier88@gmail.com").hide());
@@ -97,4 +48,3 @@ $(document).ready(function() {
     });
 
 });
->>>>>>> 4f5dc21fb2afbf1c87eb424e3d8880bd8e07bda3
